0f736aaf0819e5a6be2c4005f6d41138241c4ad5
Konstantsin Yurkevich <kutcevolov@icloud.com>
2022-07-26 09:43:37 +0300

    Commit on master

--- src/test/java/pages/BookingResultsPage.java
+++ src/test/java/pages/BookingResultsPage.java
@@ -16,31 +16,9 @@ public class BookingResultsPage extends BasePage {
             "'sr_property_block_main_row')]//div[@class = \"bui-review-score__badge\"]";
 
 
-    @FindBy(xpath = "//span[contains(@class,'sr-hotel__name')]")
-    private List<WebElement> resultsHotels;
-
-    @FindBy(xpath = "//h1[contains(text(),' properties found')]")
-    private WebElement resultOfSearch;
-
 
     public BookingResultsPage(WebDriver driver) {
         super(driver);
-    }
-
-    public List<String> getResults() {
-        List<String> list = resultsHotels.stream().map(result -> result.getAttribute("innerText"))
-                .filter(result -> !result.isEmpty()).collect(Collectors.toList());
-        List<String> strings = new ArrayList<>();
-        for (String s : list) {
-            String str = s.substring(0, s.length() - 1);
-            strings.add(str);
-        }
-        return strings;
-
-    }
-
-    public boolean isPageOpened() {
-        return resultOfSearch.isDisplayed();
     }
 
     public String getRatingFor(String hotelName) {
